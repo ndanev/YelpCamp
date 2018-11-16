8d151c2094fc628db2937e9370d441edf2ad1574
Nemanja Danev <nemanja.danev.93@gmail.com>
2018-11-16 14:25:37 -0500

    first commit

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs');
+
+
+// landing page (root path)
 app.get('/', (req, res) => {
-    res.send('Home Page');
+    res.render('landing');
+
+});
+
+// campgrounds page
+app.get('/campgrounds', (req, res) => {
+    let campgrounds = [
+        {name:"Salmon Creek", image:"https://images.pexels.com/photos/1309586/pexels-photo-1309586.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"},
+        {name:"Granite Hill", image:"https://images.pexels.com/photos/1309584/pexels-photo-1309584.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"},
+        {name:"Mountain Goat's Rest", image:"https://images.pexels.com/photos/756780/pexels-photo-756780.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"}
+    ];
+    res.render('campgrounds', {campgrounds: campgrounds});
 });
 
 app.listen(3000, () => {
