d0103ee7e75a282fe92d5ccb4a5fd3cbe582e1df
Nemanja Danev <nemanja.danev.93@gmail.com>
2018-11-17 15:57:07 -0500

    style form input elements using bootstrap 4 and setup mongoose

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+// setup mongoose
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/yelp_camp');
+
 // setup body-parser 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
