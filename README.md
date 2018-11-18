e473d14a09eb5ed2bd30ca37cc24baf4ac2bb298
Nemanja Danev <nemanja.danev.93@gmail.com>
2018-11-18 15:36:22 -0500

    change name campgrounds.ejs to index.ejs and added show page

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # YelpCamp
 YelpCamp project
+
+RESTFUL ROUTES
+
+name    URL                 Method  desc.
+=========================================================================
+INDEX   /campgrounds        GET     Display list of campgrounds
+NEW     /campgrounds/new    GET     Display form to make a new campground
+CREATE  /campgrounds        POST    Add new campgrounds to database
+SHOW    /campgrounds/:id    GET     Show info about one campground
+
--- app.js
+++ app.js
@@ -3,26 +3,52 @@ const app = express();
 
 // setup mongoose
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/yelp_camp');
+mongoose.connect('mongodb://localhost/yelp_camp', { useNewUrlParser: true });
+
+const Schema = mongoose.Schema;
+
+let campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+let Campground = mongoose.model("Campground", campgroundSchema);
+
+//     Campground.create(
+
+//         {
+//             name: "Granite Hill",
+//             image: "https://images.pexels.com/photos/1309584/pexels-photo-1309584.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+//             description: "This is huge campground. No bathrooms, no water. Beautiful granite"
+//         }, function (error, campground) {
+//             if (error) {
+//                 console.log(error);
+//             } else {
+//                 console.log('created new campground');
+//                 console.log(campground);
+//             }
+//         }
+//     );
 
 // setup body-parser 
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
 
-let campgrounds = [
-    {name:"Salmon Creek", image:"https://images.pexels.com/photos/1309586/pexels-photo-1309586.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"},
-    {name:"Granite Hill", image:"https://images.pexels.com/photos/1309584/pexels-photo-1309584.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"},
-    {name:"Mountain Goat's Rest", image:"https://images.pexels.com/photos/878251/pexels-photo-878251.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"},
-    {name:"Salmon Creek", image:"https://images.pexels.com/photos/1309586/pexels-photo-1309586.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"},
-    {name:"Granite Hill", image:"https://images.pexels.com/photos/1309584/pexels-photo-1309584.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"},
-    {name:"Mountain Goat's Rest", image:"https://images.pexels.com/photos/878251/pexels-photo-878251.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"},
-    {name:"Salmon Creek", image:"https://images.pexels.com/photos/1309586/pexels-photo-1309586.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"},
-    {name:"Granite Hill", image:"https://images.pexels.com/photos/1309584/pexels-photo-1309584.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"},
-    {name:"Mountain Goat's Rest", image:"https://images.pexels.com/photos/878251/pexels-photo-878251.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"}
-];
+// let campgrounds = [
+//     { name: "Salmon Creek", image: "https://images.pexels.com/photos/1309586/pexels-photo-1309586.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" },
+//     { name: "Granite Hill", image: "https://images.pexels.com/photos/1309584/pexels-photo-1309584.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" },
+//     { name: "Mountain Goat's Rest", image: "https://images.pexels.com/photos/878251/pexels-photo-878251.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" },
+//     { name: "Salmon Creek", image: "https://images.pexels.com/photos/1309586/pexels-photo-1309586.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" },
+//     { name: "Granite Hill", image: "https://images.pexels.com/photos/1309584/pexels-photo-1309584.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" },
+//     { name: "Mountain Goat's Rest", image: "https://images.pexels.com/photos/878251/pexels-photo-878251.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" },
+//     { name: "Salmon Creek", image: "https://images.pexels.com/photos/1309586/pexels-photo-1309586.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" },
+//     { name: "Granite Hill", image: "https://images.pexels.com/photos/1309584/pexels-photo-1309584.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" },
+//     { name: "Mountain Goat's Rest", image: "https://images.pexels.com/photos/878251/pexels-photo-878251.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" }
+// ];
 
 
 // landing page (root path)
@@ -31,29 +57,58 @@ app.get('/', (req, res) => {
 
 });
 
-// campgrounds page
+//  display list of campgrounds 
 app.get('/campgrounds', (req, res) => {
 
-    res.render('campgrounds', {campgrounds: campgrounds});
+    Campground.find({}, function(error, allCampgrounds) {
+        if(error) {
+            console.log(error);
+        } else {
+            res.render('index', { campgrounds: allCampgrounds });
+        }
+    })
+
 });
 
-// post request --> create new campgrounds
+// post request --> add newe campgrounds to database
 app.post('/campgrounds', (req, res) => {
-    //get data from form and add to campground array
+    //get data from form
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name: name, image: image};
-    campgrounds.push(newCampground);
-    //redirect to campground page
-    res.redirect('/campgrounds');
-    
+    var description = req.body.description
+    var newCampground = { name: name, image: image, description: description};
+    // Create new campground and save to the database
+    Campground.create(newCampground, (error, newlyCreated) => {
+        if(error) {
+            console.log(error);
+        } else {
+            //redirect to campground page
+            res.redirect('/campgrounds');
+        }
+    });
 });
 
-// show the form where we send data to post route 
+// show the form to create new campgrounds 
 app.get('/campgrounds/new', (req, res) => {
     res.render('new');
 });
 
+// show info about one dog
+app.get('/campgrounds/:id', (req, res) => {
+    // find the campground with provided by ID
+    Campground.findById(req.params.id, function(error, foundCampground) {
+        if(error) {
+            console.log(error);
+        } else {
+            // render show tamplate with that campground
+            res.render('show', {campground: foundCampground});
+        }
+    });
+
+});
+
+
+// Listening on port 3000 
 app.listen(3000, () => {
     console.log('YelpCamp server has started...');
 });
